02945581933ac8eae06f01fcbdb73bef94587a95
Carlos F. Martínez Sánchez <carlos.f.martinez.s+github@gmail.com>
2025-04-25 20:01:47 -0400

    se añaden métodos para reemplazar libros en el controlador, incluyendo búsqueda por ISBN y por índice

--- book-store/src/main/java/cl/duoc/cmartinez/bookstore/controller/BookController.java
+++ book-store/src/main/java/cl/duoc/cmartinez/bookstore/controller/BookController.java
@@ -47,4 +47,52 @@ public class BookController {
     books.add(request);
     return ResponseEntity.status(HttpStatus.CREATED).build();
   }
+
+  @PutMapping("/{isbn}")
+  public ResponseEntity<MessageResponse> replaceBook(
+          @PathVariable String isbn,
+          @RequestBody Book request) {
+    int count = 0;
+    for (Book book : books) {
+      if (book.getIsbn().equals(isbn)) {
+        break;
+      }
+      count++; // count = count + 1
+    }
+
+    if (count == books.size()) {
+      return ResponseEntity.notFound().build();
+    }
+
+    books.set(count, request);
+
+    return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(new MessageResponse("Book was replaced"));
+  }
+
+  @PutMapping("/{isbn}/index")
+  public ResponseEntity<MessageResponse> replaceBookIndex(
+          @PathVariable String isbn,
+          @RequestBody Book request) {
+    Book found = null;
+    for (Book book : books) {
+      if (book.getIsbn().equals(isbn)) {
+        found = book;
+        break;
+      }
+    }
+
+    int index = books.indexOf(found);
+
+    if (index == -1) {
+      return ResponseEntity.notFound().build();
+    }
+
+    books.set(index, request);
+
+    return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(new MessageResponse("Book was replaced"));
+  }
 }
