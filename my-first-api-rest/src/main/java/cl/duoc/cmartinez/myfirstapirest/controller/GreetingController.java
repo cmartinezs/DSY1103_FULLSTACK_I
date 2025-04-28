1f6ffc3c9016af8a1a5dea6fe18c512eaa899e93
Carlos F. Martínez Sánchez <carlos.f.martinez.s+github@gmail.com>
2025-04-28 15:59:16 -0400

    se añade el método POST para crear saludos y el método DELETE para eliminar saludos en el controlador de saludos; se añade la clase IdResponse

--- my-first-api-rest/src/main/java/cl/duoc/cmartinez/myfirstapirest/controller/GreetingController.java
+++ my-first-api-rest/src/main/java/cl/duoc/cmartinez/myfirstapirest/controller/GreetingController.java
@@ -2,6 +2,7 @@ package cl.duoc.cmartinez.myfirstapirest.controller;
 
 import cl.duoc.cmartinez.myfirstapirest.controller.request.GreetingRequest;
 import cl.duoc.cmartinez.myfirstapirest.controller.response.GreetingResponse;
+import cl.duoc.cmartinez.myfirstapirest.controller.response.IdResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,79 +13,92 @@ import java.util.List;
 @RestController
 @RequestMapping("/greetings")
 public class GreetingController {
-    private List<GreetingResponse> greetingResponses;
+  private List<GreetingResponse> greetingResponses;
 
-    public GreetingController() {
-        greetingResponses = new ArrayList<>();
-        greetingResponses.add(new GreetingResponse(1, "Hello World"));
-        greetingResponses.add(new GreetingResponse(2, "Bye World"));
-        greetingResponses.add(new GreetingResponse(3, "My World"));
-        greetingResponses.add(new GreetingResponse(4, "Your World"));
-        greetingResponses.add(new GreetingResponse(5, "Cruel World"));
-        greetingResponses.add(new GreetingResponse(6, "Sayonara World"));
-        greetingResponses.add(new GreetingResponse(7, "Hi World"));
-        greetingResponses.add(new GreetingResponse(8, "First World"));
-        greetingResponses.add(new GreetingResponse(9, "Sad World"));
-        greetingResponses.add(new GreetingResponse(10, "Pretty World"));
+  public GreetingController() {
+    greetingResponses = new ArrayList<>();
+    greetingResponses.add(new GreetingResponse(1, "Hello World"));
+    greetingResponses.add(new GreetingResponse(2, "Bye World"));
+    greetingResponses.add(new GreetingResponse(3, "My World"));
+    greetingResponses.add(new GreetingResponse(4, "Your World"));
+    greetingResponses.add(new GreetingResponse(5, "Cruel World"));
+    greetingResponses.add(new GreetingResponse(6, "Sayonara World"));
+    greetingResponses.add(new GreetingResponse(7, "Hi World"));
+    greetingResponses.add(new GreetingResponse(8, "First World"));
+    greetingResponses.add(new GreetingResponse(9, "Sad World"));
+    greetingResponses.add(new GreetingResponse(10, "Pretty World"));
+  }
+
+  @GetMapping
+  public ResponseEntity<List<GreetingResponse>> getGreetings() {
+    return ResponseEntity.ok(greetingResponses);
+  }
+
+  @GetMapping("/{elementNumber}")
+  public ResponseEntity<GreetingResponse> getGreeting(@PathVariable int elementNumber) {
+    if (elementNumber >= 0 && elementNumber < greetingResponses.size()) {
+      return ResponseEntity.ok(greetingResponses.get(elementNumber));
     }
 
-    @GetMapping
-    public ResponseEntity<List<GreetingResponse>> getGreetings() {
-        return ResponseEntity.ok(greetingResponses);
+    return ResponseEntity.notFound().build();
+  }
+
+  @PutMapping("/{elementNumber}")
+  public ResponseEntity<GreetingResponse> putGreeting(
+      @PathVariable int elementNumber, @RequestBody GreetingRequest request) {
+
+    if (elementNumber >= 0 && elementNumber < greetingResponses.size()) {
+
+      GreetingResponse element = greetingResponses.get(elementNumber);
+      element.setMessage(request.getMessage());
+
+      return ResponseEntity.ok(element);
     }
 
-    @GetMapping("/{elementNumber}")
-    public ResponseEntity<GreetingResponse> getGreeting(
-            @PathVariable int elementNumber) {
-        if(elementNumber >= 0 &&
-                elementNumber < greetingResponses.size()) {
-            return ResponseEntity.ok(
-                    greetingResponses.get(elementNumber));
-        }
+    return ResponseEntity.notFound().build();
+  }
 
-        return ResponseEntity.notFound().build();
+  @PostMapping
+  public ResponseEntity<IdResponse> postGreeting(@RequestBody GreetingRequest request) {
+    int nextId = greetingResponses.size() + 1;
+    greetingResponses.add(new GreetingResponse(nextId, request.getMessage()));
+    return ResponseEntity.status(HttpStatus.CREATED).body(new IdResponse(nextId));
+  }
+
+  @DeleteMapping("/{elementNumber}")
+  public ResponseEntity<GreetingResponse> deleteGreeting(@PathVariable int elementNumber) {
+
+    if (elementNumber >= 0 && elementNumber < greetingResponses.size()) {
+
+      GreetingResponse element = greetingResponses.get(elementNumber);
+      greetingResponses.remove(element);
+
+      return ResponseEntity.noContent().build();
     }
 
-    @PutMapping("/{elementNumber}")
-    public ResponseEntity<GreetingResponse> putGreeting(
-            @PathVariable int elementNumber,
-            @RequestBody GreetingRequest request) {
+    return ResponseEntity.notFound().build();
+  }
 
-        if(elementNumber >= 0 &&
-                elementNumber < greetingResponses.size()) {
+  @GetMapping("/hello")
+  public ResponseEntity<GreetingResponse> hello() {
+    var response = new GreetingResponse();
+    response.setMessage("Hello World");
+    return ResponseEntity.ok(response);
+  }
 
-            GreetingResponse element = greetingResponses.get(elementNumber);
-            element.setMessage(request.getMessage());
+  @GetMapping("/hello-guest")
+  public ResponseEntity<GreetingResponse> helloGuest(
+      @RequestParam(value = "name") String name,
+      @RequestParam(value = "lastname") String lastname) {
+    var response = new GreetingResponse();
+    response.setMessage("Hello " + name + " " + lastname);
+    return ResponseEntity.ok(response);
+  }
 
-            return ResponseEntity.ok(element);
-        }
-
-        return ResponseEntity.notFound().build();
-    }
-
-
-    @GetMapping("/hello")
-    public ResponseEntity<GreetingResponse> hello() {
-        var response = new GreetingResponse();
-        response.setMessage("Hello World");
-        return ResponseEntity.ok(response);
-    }
-
-    @GetMapping("/hello-guest")
-    public ResponseEntity<GreetingResponse> helloGuest(
-            @RequestParam(value = "name") String name,
-            @RequestParam(value = "lastname") String lastname) {
-        var response = new GreetingResponse();
-        response.setMessage("Hello " + name + " " + lastname);
-        return ResponseEntity.ok(response);
-    }
-
-    @GetMapping("/goodbye")
-    public ResponseEntity<GreetingResponse> goodbye() {
-        var response = new GreetingResponse();
-        response.setMessage("Goodbye cruel World");
-        return ResponseEntity.ok(response);
-    }
-
-
+  @GetMapping("/goodbye")
+  public ResponseEntity<GreetingResponse> goodbye() {
+    var response = new GreetingResponse();
+    response.setMessage("Goodbye cruel World");
+    return ResponseEntity.ok(response);
+  }
 }
---
+++ my-first-api-rest/src/main/java/cl/duoc/cmartinez/myfirstapirest/controller/response/IdResponse.java
@@ -0,0 +1,14 @@
+package cl.duoc.cmartinez.myfirstapirest.controller.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class IdResponse {
+    private int id;
+}
