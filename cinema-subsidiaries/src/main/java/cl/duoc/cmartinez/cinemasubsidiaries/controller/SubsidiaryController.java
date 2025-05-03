2590af6941be0ad5a75dd1ba29daab1a3e0c911b
Carlos F. Martínez Sánchez <carlos.f.martinez.s+github@gmail.com>
2025-05-02 21:54:02 -0400

    se mejora el controlador de sucursales para manejar respuestas HTTP; se añaden métodos para actualizar y eliminar sucursales

--- cinema-subsidiaries/src/main/java/cl/duoc/cmartinez/cinemasubsidiaries/controller/SubsidiaryController.java
+++ cinema-subsidiaries/src/main/java/cl/duoc/cmartinez/cinemasubsidiaries/controller/SubsidiaryController.java
@@ -1,6 +1,8 @@
 package cl.duoc.cmartinez.cinemasubsidiaries.controller;
 
 import cl.duoc.cmartinez.cinemasubsidiaries.domain.Subsidiary;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -30,17 +32,53 @@ public class SubsidiaryController {
   }
 
   @GetMapping("/{id}")
-  public Subsidiary getSubsidiary(@PathVariable int id) {
+  public ResponseEntity<Subsidiary> getSubsidiary(@PathVariable int id) {
     for (Subsidiary subsidiary : subsidiaries) {
       if (subsidiary.getId() == id) {
-        return subsidiary;
+        return ResponseEntity.ok(subsidiary);
       }
     }
-    return null;
+    return ResponseEntity.notFound().build();
   }
 
   @PostMapping
-  public void addSubsidiary(@RequestBody Subsidiary subsidiary) {
+  public ResponseEntity<Void> addSubsidiary(@RequestBody Subsidiary subsidiary) {
     subsidiaries.add(subsidiary);
+    return ResponseEntity.status(HttpStatus.CREATED).build();
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<Void> updateSubsidiary(
+          @PathVariable int id,
+          @RequestBody Subsidiary request) {
+    Subsidiary found = null;
+    for (Subsidiary subsidiary : subsidiaries) {
+      if (subsidiary.getId() == id) {
+        found = subsidiary;
+      }
+    }
+
+    if (found != null) {
+      int index = subsidiaries.indexOf(found);
+      subsidiaries.set(index, request);
+      return ResponseEntity.noContent().build();
+    }
+
+    return ResponseEntity.notFound().build();
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Void> deleteSubsidiary(@PathVariable int id) {
+    Subsidiary found = null;
+    for (Subsidiary subsidiary : subsidiaries) {
+      if (subsidiary.getId() == id) {
+        found = subsidiary;
+      }
+    }
+    if (found != null) {
+      subsidiaries.remove(found);
+      return ResponseEntity.noContent().build();
+    }
+    return ResponseEntity.notFound().build();
   }
 }
